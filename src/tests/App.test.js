f18fda2166a26e87321190eb49009c08acd89568
Philip Lages <psilvaelages@gmail.com>
2022-08-16 15:13:12 -0300

    Starting the project

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from '../App';
+
+describe('Testing the "App" component', () => {
+  it('should contain a fixed number of navigation links', () => {
+    render(<App />);
+  });
+});
