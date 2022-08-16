5869c7c49ed3f24e49b013db2ddbbcca8419d0ba
Philip Lages <psilvaelages@gmail.com>
2022-08-16 17:54:37 -0300

    feat: add requirement 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helper/renderWithRouter';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+describe('Testing the "FavoritePokemons" component', () => {
+  it('should have a message if there is no favorite pokémon', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const noPokemonMsg = screen.getByText(/No favorite pokemon found/i);
+
+    expect(noPokemonMsg).toBeInTheDocument();
+  });
+});
