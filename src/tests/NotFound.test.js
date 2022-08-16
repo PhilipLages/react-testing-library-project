a8caa1d1a5c7ecd4c4f6e623dfaea4098c09952b
Philip Lages <psilvaelages@gmail.com>
2022-08-16 18:04:13 -0300

    feat: add requirement 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helper/renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('Testing the "NotFound" component', () => {
+  it('should contain a heading with "Page requested not found"', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundTitle = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2,
+    });
+
+    expect(notFoundTitle).toBeInTheDocument();
+  });
+
+  it('should contain an image for no pokémons found', () => {
+    renderWithRouter(<NotFound />);
+
+    const URL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const imgAlt = 'Pikachu crying because the page requested was not found';
+
+    const img = screen.getByAltText(imgAlt);
+
+    expect(img.src).toBe(URL);
+  });
+});
